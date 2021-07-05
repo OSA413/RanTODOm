aae0f3df63ad4533b86f33a5e59d5b61fe401271
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2021-07-05 18:50:39 +0500

    Clean up and SCSS

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello world!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
