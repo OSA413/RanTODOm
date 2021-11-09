050ba89ffa4c6353af7399bf74d548e63c712a49
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2021-11-10 04:05:04 +0500

    Update header.tsx

--- src/app/header/header.tsx
+++ src/app/header/header.tsx
@@ -5,7 +5,8 @@ const donate = () => {
 const Header = () => {
     return (
         <div className="w-full h-16 mb-4 flex bg-blue-400 justify-between">
-            <div className="text-white font-bold self-center w-16 text-center text-5xl"></div>
+            <div className="text-white font-bold self-center text-center text-xl mx-4 p-2 bg-blue-700 rounded-lg"
+            >⚙ Settings</div>
             <div className="text-white font-bold self-center text-center text-5xl">RanTODOm</div>
             <div className="text-white font-bold self-center text-center text-xl mx-4 p-2 bg-red-400 rounded-lg"
             onClick={donate}>☕ Donate</div>
